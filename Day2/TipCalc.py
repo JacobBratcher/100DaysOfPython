e91cfcd0e5b0cd5ee2fd8d3d15d8aaf68918ade6
Jacob Bratcher <jacobwbratcher@gmail.com>
2024-01-04 16:47:27 -0500

    update

--- Day2/TipCalc.py
+++ Day2/TipCalc.py
@@ -10,10 +10,14 @@ tip_percent = input("What percentage tip would you like to give? 10, 12, or 15? 
 # accepts user input for how many people are spliting the bill
 people = input("How many people to split the bill?" )
 
+# converts whole number given by user input into decimal 
 tip_decimal = int(tip_percent) / 100 
 
+# calcualtes total bill with tip 
 total = (float(bill) * float(tip_decimal)) + float(bill)  
 
+# splits the bill among how many people the user inputs 
 split_total = "{:.2f}".format(float(total) / int(people))
 
+# prints how much each person has to pay 
 print(f"Each person should pay: {split_total}")
--- Day2/WeekCalc.py
+++ Day2/WeekCalc.py
@@ -6,8 +6,6 @@ weeks_total = int(4680)
 
 weeks_spent = int(age) * 52
 
-print(weeks_spent)
-
 weeks_left = int(weeks_total - weeks_spent)
 
 print(f"You have {weeks_left} weeks left.")
