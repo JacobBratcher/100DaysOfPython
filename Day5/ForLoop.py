b1efaf3c2330147f0f8f1cda66e8883a68db4022
Jacob Bratcher <137185518+JacobBratcher@users.noreply.github.com>
2024-01-18 13:41:43 -0500

    Delete Day5 directory
    Reorganizing repo

--- Day5/ForLoop.py
+++
@@ -1,9 +0,0 @@
-# for loop that prints each fruit in the list to the console
-fruits = ["Apple", "Peach", "Pear"]
-for fruit in fruits:
-    print(fruit)
-    
-# prints to console with Pie after each item in the list 
-    print(fruit + " Pie")
-
-# indentions matter, lines inside of the loop will execute over and over 
--- Day5/RangeFunction.py
+++
@@ -1,9 +0,0 @@
-# prints each number from 1 - 10 in the console 
-# ends on the number before the end of your range 
-for number in range(1, 11):
-    print(number)
-    
-total = 0 
-for number in range(1, 101):
-    total += number
-print(total)
